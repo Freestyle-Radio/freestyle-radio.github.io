d9e4419a19de513f9ab7257db42bd5c9975b24b2
Freestyle-Radio <163935368+Freestyle-Radio@users.noreply.github.com>
2025-05-18 00:52:12 +0300

    Create vol-radio.js
    Added volume control functionality

---
+++ js/vol-radio.js
@@ -0,0 +1,9 @@
+<script>
+    const audio = document.getElementById('audio');
+    const volumeSlider = document.getElementById('volume');
+
+    // Установка громкости при изменении ползунка
+    volumeSlider.addEventListener('input', () => {
+      audio.volume = volumeSlider.value;
+    });
+</script>
