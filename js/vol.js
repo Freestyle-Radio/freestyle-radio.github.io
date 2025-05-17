552cdda34c3b439c761f2019b0e697021d5e7bf6
Freestyle-Radio <163935368+Freestyle-Radio@users.noreply.github.com>
2025-05-18 01:09:15 +0300

    Update vol.js

--- js/vol.js
+++ js/vol.js
@@ -1,10 +1,10 @@
-function getAllAudioTags() {
-    return [...document.querySelectorAll('audio')]
-}
+// function getAllAudioTags() {
+//     return [...document.querySelectorAll('audio')]
+// }
 
-getAllAudioTags().forEach((item, index, arr) => {
-    item.onvolumechange = (e) => arr.forEach((el) => {
-        if(el === e.target) return
-        el.volume = item.volume
-    })
-})
+// getAllAudioTags().forEach((item, index, arr) => {
+//     item.onvolumechange = (e) => arr.forEach((el) => {
+//         if(el === e.target) return
+//         el.volume = item.volume
+//     })
+// })
