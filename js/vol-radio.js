4d76140847e67561d2ea7f5f906cce41c74cd48f
Freestyle-Radio <163935368+Freestyle-Radio@users.noreply.github.com>
2025-05-21 13:34:55 +0300

    Update vol-radio.js

--- js/vol-radio.js
+++ js/vol-radio.js
@@ -1,12 +1,11 @@
 const audio = document.getElementById('audio');
 const volumeSlider = document.getElementById('volume');
 
-// Установка начальной громкости
-audio.volume = 0.3;
-
 // Проверка ширины экрана: включаем управление только на ноутах и ПК
 if (window.innerWidth >= 768) {
-  volumeSlider.addEventListener('input', () => {
+// Установка начальной громкости
+    audio.volume = 0.3;
+    volumeSlider.addEventListener('input', () => {
     audio.volume = volumeSlider.value;
   });
 }
