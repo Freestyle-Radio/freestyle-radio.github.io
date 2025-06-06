4f6406d1072b4a010fda638aa060805bccc09267
Freestyle-Radio <163935368+Freestyle-Radio@users.noreply.github.com>
2025-06-06 20:19:52 +0300

    Update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,1 +1,14 @@
+const modal = document.getElementById("modal");
+const hamburger = document.getElementById("hamburger");
+const closeModal = document.getElementById("closeModal");
+
+hamburger.addEventListener("click", () => {
+  modal.classList.add("active");
+  document.body.classList.add("modal-open");
+});
+
+closeModal.addEventListener("click", () => {
+  modal.classList.remove("active");
+  document.body.classList.remove("modal-open");
+});
 
