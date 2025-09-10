8300f446a5ce934eb69695e3b1fbb2eb5218f7e7
Freestyle-Radio <freestyle.digital.radio@gmail.com>
2025-09-11 01:53:03 +0300

    New version site

--- js/accordion.js
+++
@@ -1,7 +0,0 @@
-// Находим кнопку с вопросом
-const question = document.querySelector('.question');
-// Добавляем слушатель на клик 
-question.addEventListener('click', ({ target }) => {
-  // Меняем состояние кнопки
-  target.classList.toggle('is-active');
-});
